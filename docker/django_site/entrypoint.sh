b36d996ba42156b0a757adedbcf31a924b63d37f
Katisvektavir <Katisvektavir@gmail.com>
2026-09-18 02:37:05 +0300

    change docker and nginx configuration for share media and build project on raspberry pi 1

---
+++ docker/django_site/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+while ! nc -z db 5432; do
+  sleep 0.5
+done
+
+echo "bd started"
+
+python manage.py migrate --noinput
+
+echo $@
+
+exec "$@"
--- main/settings/django_settings.py
+++ main/settings/django_settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG').lower() == 'true'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
-
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(',')
 
 INSTALLED_APPS = [
     'bootstrap5',
